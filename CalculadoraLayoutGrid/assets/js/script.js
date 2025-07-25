b9c43e9091defa2512add966bde2de83bd5513f6
Jonathan Rezende dos Santos <161469965+DevSanttos@users.noreply.github.com>
2025-07-25 09:56:15 -0300

    Implementações de funcionalidade adicionais

--- CalculadoraLayoutGrid/assets/js/script.js
+++ CalculadoraLayoutGrid/assets/js/script.js
@@ -10,27 +10,50 @@ document.addEventListener("DOMContentLoaded", () => {
 
         const type = click.dataset.type;
         const value = click.dataset.value;
-
+        
         if(type === 'limpar') {
             inputDisplay.value = '';
             return;
         }
 
+        if(type === 'deletar') {
+            const comprimentoInputDisplay = inputDisplay.value.length;
+            alert(inputDisplay.value.slice());
+            // inputDisplay.value = inputDisplay.value.slice(-1);
+        }
+
         if (type === 'numero') {
             inputDisplay.value += value;
+            isOperador = false;
         }
-        
-        if (type === 'operador') {
-            if(inputDisplay.value === '') {
-                return;
-            }
-            
-            const lastChar = inputDisplay.value.slice(-1);
-      
-            if(typeof lastChar === '') {
-                alert('Você não pode inserir dois operadores seguidos.');
+
+        if (type === 'decimal') {
+            const armazenaTxtInputDisplay = inputDisplay.value;
+            let temPonto = false;
+            for(let i = 0; i < armazenaTxtInputDisplay.length; i++) {
+                if(armazenaTxtInputDisplay[i] === '.') {
+                    temPonto = true;
+                    break;
+                } 
             }
 
+            if(temPonto === false) {
+                inputDisplay.value += value;
+            }
+        }
+        
+        if(type === 'operador' && isOperador === false) {
+            inputDisplay.value += value;
+        }
+
+        if(type === 'operador') {
+            isOperador = true;
+        }
+
+        // 55*2+3/4
+        if(type === 'igual') {
+            let partes = inputDisplay.value.split('*');
+            alert(partes);
         }
     });
 });
